52fd2a92afd32feba82131d1db27de49fcc7021d
Brittany Pinder <brittpinder@gmail.com>
2020-07-08 18:50:00 +0200

    Using FontAwesome and displaying checkmarks for months that critters are available

--- src/components/Navigation/TopBar/TopBar.js
+++ src/components/Navigation/TopBar/TopBar.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Nav, Navbar } from 'react-bootstrap';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLeaf } from '@fortawesome/free-solid-svg-icons'
+
 const topBar = () => (
     <Navbar bg="dark" variant="dark">
-        <Navbar.Brand as={Link} to="/">Animal Crossing</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/"><FontAwesomeIcon icon={faLeaf}/> Animal Crossing</Navbar.Brand>
             <Nav className="mr-auto">
                 <Nav.Link as={Link} to="/fish" >Fish</Nav.Link>
                 <Nav.Link as={Link} to="/bugs" >Bugs</Nav.Link>
--- src/components/Tables/CritterTable/CritterTable.js
+++ src/components/Tables/CritterTable/CritterTable.js
@@ -1,27 +1,43 @@
 import React from 'react';
-import Table from 'react-bootstrap/Table'
+import Table from 'react-bootstrap/Table';
 
-const critterTable = (props) => (
-    <Table>
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Image</th>
-                <th>Price</th>
-                <th>Location</th>
-            </tr>
-        </thead>
-        <tbody>
-            {props.critterData.map(critter => (
-                <tr key={critter.name}>
-                    <td>{critter.name}</td>
-                    <td><img src={require("../../../" + critter.image)} /></td>
-                    <td>{critter.price}</td>
-                    <td>{critter.location}</td>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+
+import TimeUtil from '../../../scripts/timeUtil';
+
+const critterTable = (props) => {
+    const monthHeaders = [];
+    for (const month of TimeUtil.months) {
+        monthHeaders.push(<th>{month.shortName}</th>)
+    }
+
+    return (
+        <Table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Image</th>
+                    <th>Price</th>
+                    <th>Location</th>
+                    {monthHeaders}
                 </tr>
-            ))}
-        </tbody>
-    </Table>
-);
+            </thead>
+            <tbody>
+                {props.critterData.map(critter => (
+                    <tr key={critter.name}>
+                        <td>{critter.name}</td>
+                        <td><img src={require("../../../" + critter.image)} /></td>
+                        <td>{critter.price}</td>
+                        <td>{critter.location}</td>
+                        {TimeUtil.months.map((month) => (
+                            <td>{critter.months.includes(month.id) ? <FontAwesomeIcon icon={faCheck} /> : null}</td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
+    );
+}
 
 export default critterTable;
