26e173d7a4993930814744fff7c31b370102d7ad
Brittany Pinder <brittpinder@gmail.com>
2020-07-12 11:09:54 +0200

    Added footer with information to CritterCard

---
+++ src/components/BellPrice/BellPrice.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './BellPrice.module.css';
+
+import coin from '../../assets/images/icons/coin.png';
+
+const bellPrice = (props) => (
+    <div className={styles.BellPrice}>
+        <img src={coin} />
+        <span>{props.price}</span>
+    </div>
+);
+
+export default bellPrice;
--- src/components/Cards/CritterCard/CritterCard.js
+++ src/components/Cards/CritterCard/CritterCard.js
@@ -2,15 +2,29 @@ import React from 'react'
 
 import styles from './CritterCard.module.css';
 
-import Card from 'react-bootstrap/Card';
+import BellPrice from '../../BellPrice/BellPrice';
+
+import { Card, Row, Col } from 'react-bootstrap';
 
 const critterCard = (props) => (
     <Card bg="light" className={styles.CritterCard}>
         <Card.Img variant="top" src={require("../../../" + props.critter.image)} />
         <Card.Body>
             <Card.Title>{props.critter.name}</Card.Title>
-            <Card.Text>{props.critter.price} Bells</Card.Text>
         </Card.Body>
+        <Card.Footer className={styles.Footer}>
+            <Row>
+                <Col>
+                    <Card.Text>{props.critter.location}</Card.Text>
+                </Col>
+                <Col>
+                    <BellPrice price={props.critter.price} />
+                </Col>
+                <Col>
+                    <Card.Text>{props.critter.timeText}</Card.Text>
+                </Col>
+            </Row>
+        </Card.Footer>
     </Card>
 );
 
