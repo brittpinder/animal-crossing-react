e1a16be48e479481829a3dcaa61460b92d446efa
Brittany Pinder <brittpinder@gmail.com>
2020-07-26 13:15:17 +0200

    TopBar now collapses on smaller resolutions

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -9,13 +9,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLeaf } from '@fortawesome/free-solid-svg-icons'
 
 const topBar = () => (
-    <Navbar variant="dark" className={styles.TopBar}>
+    <Navbar variant="dark" className={styles.TopBar} expand="md">
         <Navbar.Brand as={Link} to="/"><FontAwesomeIcon icon={faLeaf}/> Animal Crossing</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
                 <Nav.Link as={Link} to="/fish" >Fish</Nav.Link>
                 <Nav.Link as={Link} to="/bugs" >Bugs</Nav.Link>
                 <Nav.Link as={Link} to="/sea-creatures" >Sea Creatures</Nav.Link>
             </Nav>
+        </Navbar.Collapse>
     </Navbar>
 );
 
