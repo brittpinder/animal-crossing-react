d562176557eceaa521c5264657fc802ec977a184
Brittany Pinder <brittpinder@gmail.com>
2020-07-26 18:07:34 +0200

    Displaying image on hemisphere button

--- src/containers/HemisphereButton/HemisphereButton.js
+++ src/containers/HemisphereButton/HemisphereButton.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actionTypes from '../../store/actions';
 
+import styles from './HemisphereButton.module.css';
+import NorthImg from '../../assets/images/icons/north.png';
+import SouthImg from '../../assets/images/icons/south.png';
+
 import Button from 'react-bootstrap/Button';
 
 class HemisphereButton extends Component {
     render() {
-        const hemisphere = this.props.isNorthernHemisphere ? 'North' : 'South';
+        const hemisphere = this.props.isNorthernHemisphere ? 'Northern Hemisphere' : 'Southern Hemisphere';
+        const image = this.props.isNorthernHemisphere ? NorthImg : SouthImg;
         return (
-            <Button onClick={this.props.onToggleHemisphere}>{hemisphere}</Button>
+            <Button onClick={this.props.onToggleHemisphere} className={styles.HemisphereButton}>
+                <img src={image} alt={hemisphere} title={hemisphere}/>
+            </Button>
         );
     }
 }
